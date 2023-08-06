33ee8b5e460e45ffbb560f7744d41aab03495f54
karthickb2210 <116095490+karthickb2210@users.noreply.github.com>
2023-08-06 11:32:58 +0530

    Add files via upload

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import { BiHomeAlt,BiUser} from 'react-icons/bi';
-import { BsClipboardData, BsChatSquareText} from 'react-icons/bs';
+import { BiHomeAlt} from 'react-icons/bi';
+import { BsClipboardData} from 'react-icons/bs';
+import {CgProfile} from 'react-icons/cg';
 import {Link} from 'react-scroll';
+import { FiMail } from "react-icons/fi";
+import { GrGithub} from "react-icons/gr";
 const Nav = () => {
   return (
-   <nav  className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
+   <nav  className='fixed bottom-2 lg:bottom-6 w-full overflow-hidden z-50'>
   <div className='container mx-auto'>
 {}
 <div className='w-full bg-black/20 h-[66px] backdrop-blur-2xl
 rounded-full max-w-[460px] mx-auto px-5 flex 
 justify-between items-center text-2x1 text-white/50'>
-  <Link to='home' activeClass='active' smooth={true} spy={true}  offset={-230}
+  <Link to='home' activeClass='active' spy={true} offset={-230} smooth={true}
   className='cursor-pointer w-[60px] h-[60px] flex items-center
   justify-center '>
     <BiHomeAlt />
   </Link>
-  <Link to='about'  activeClass='active' smooth={true} spy={true} 
-  className='cursor-pointer w-[60px] h-[60px] flex items-center
-  justify-center'>
-    <BiUser />
-  </Link>
-  <Link to='services'  activeClass='active' smooth={true} spy={true} 
+  <Link to='Cp'  activeClass='active'  spy={true} smooth={true}
   className='cursor-pointer w-[60px] h-[60px] flex items-center
   justify-center'>
     <BsClipboardData />
   </Link>
+  <Link to='Projects'  activeClass='active'  spy={true} smooth={true}
+  className='cursor-pointer w-[60px] h-[60px] flex items-center
+  justify-center'>
+    <GrGithub />
+  </Link>
  
-  <Link to='contact'  activeClass='active' smooth={true} spy={true}
+  <Link to='contact'  activeClass='active'  spy={true} smooth={true}
    className='cursor-pointer w-[60px] h-[60px] flex items-center
   justify-center' >
-    <BsChatSquareText />
+    <FiMail />
   </Link>
+  <Link to='education'  activeClass='active'  spy={true} smooth={true}
+   className='cursor-pointer w-[60px] h-[60px] flex items-center
+  justify-center' >
+    <CgProfile />
+  </Link>
+
+  
+  
+
   
   
 </div>
